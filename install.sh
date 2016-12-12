ca53ab37d5d77b5ee93abb37e837e9274b1c08fa
Sergio Valverde <sergio@radix.imdea>
2016-12-12 09:42:30 +0100

    Fix rvm error. It should be in shell rcs not in profile.

--- install.sh
+++ install.sh
@@ -11,10 +11,11 @@ VERSION=2.0.0
 
 NAME=$0;
 MIN_RUBY_VERSION_STR="2.0.0"
-MIN_RUBY_VERSION="${MIN_RUBY_VERSION_STR//.}" # 1.9.2 -> 192
+MIN_RUBY_VERSION="${MIN_RUBY_VERSION_STR//.}" # 2.0.0 -> 200
 
 
 declare -a programsToInstall=("git" "ruby" "ruby-dev")
+declare -a shellsToCheck=("bash" "zsh")
 
 #
 # <- VARS
@@ -56,6 +57,36 @@ check_and_install_programs() {
     msg i "All gems dependecies installed."
 }
 
+
+add_rvm_to_rcs() {
+    for shell_i in "$@" ; do
+        # include $shell_I rc if it exists
+        RC="."$shell_i"rc"
+        PWDRC="$HOME/$RC"
+        RVMLINE='[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*'
+        if [ -f $PWDRC ]; then
+            if ! grep -Fxq "$RVMLINE" $PWDRC ; then
+                echo $RVMLINE >> $PWDRC
+            fi
+        fi
+    done
+}
+
+make_ruby_vm() {
+    # Create a virtual env with ruby 2.0.0
+    # install rvm for manage the ruby version
+    #   commands from http://gillesfabio.com/blog/2011/03/01/rvm-for-pythonistas-virtualenv-for-rubyists/
+    #   more info https://rvm.io/rvm/install
+    bash < <( curl http://rvm.beginrescueend.com/releases/rvm-install-head )
+    add_rvm_to_rcs ${shellsToCheck[@]}
+    source ~/.rvm/scripts/rvm
+    pw=$PWD
+    cd ..
+    rvm use 2.0.0@imdea-controls --create
+    cd $pw
+}
+
+
 #
 # <- FUNCTIONS
 #
@@ -63,31 +94,23 @@ check_and_install_programs() {
 
 
 main() {
-    ruby_version_long=$(ruby --version)
-    ruby_version_short="$(echo $ruby_version_long | awk '{print $2}')"
-    ruby_version_num=${ruby_version_short:0:5}
-    ruby_version_num_int=${ruby_version_num//.}
-
-
     # Check if ruby is installed and install
     check_and_install_programs ${programsToInstall[@]}
 
 
+    ruby_version_long=$(ruby --version)
+    ruby_version_short="$(echo $ruby_version_long | awk '{print $2}')"
+    ruby_version_num=${ruby_version_short:0:5}
+    ruby_version_num_int=${ruby_version_num//.} # Example: 2.1.5 -> 215
+
     # Check ruby version
-    if [ "$ruby_version_num_int" -lt "$MIN_RUBY_VERSION" ] ; then
-        msg e "Your ruby version is les than the minimun required... yours=$ruby_version_num < min=$MIN_RUBY_VERSION_STR."
-        exit 1
+    if [ "$ruby_version_num_int" -ne "$MIN_RUBY_VERSION" ] ; then
+        msg w "Your ruby version is not the version required... yours=$ruby_version_num != min=$MIN_RUBY_VERSION_STR."
+        msg i "We have to make a virtual env and install rvm for manage the ruby version."
+        make_ruby_vm
+    else
+        msg i "Your ruby version is correct... $ruby_version_num == $MIN_RUBY_VERSION_STR"
     fi
-    msg i "Your ruby version is correct... $ruby_version_num > $MIN_RUBY_VERSION_STR"
-
-
-    # Create a virtual env with ruby 2.0.0 # install rvm
-    #   commands from http://gillesfabio.com/blog/2011/03/01/rvm-for-pythonistas-virtualenv-for-rubyists/
-    #   more info https://rvm.io/rvm/install
-    bash < <( curl http://rvm.beginrescueend.com/releases/rvm-install-head )
-    echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"' >> ~/.profile
-    source ~/.rvm/scripts/rvm
-    rvm use 2.0.0@imdea-controls --create
 
     # Install dependecies gems
     gem install bundler
